2a554fe0cf1bb3d434d9314538362d47314f30e5
laurabenites <128058525+laurabenites@users.noreply.github.com>
2023-10-06 12:11:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function funcaocorrigir() {
+    var respostaCerta = "correta";
+    var repostaUsuario = document.querySelector ('input[name="reposta":checked');
+
+    if (repostaUsuario) {
+        if (repostaUsuario.value === respostaCerta) {
+            document.getElementById("retorno").innerHTML ="Acertou, mizeravel";
+        }
+        ele{
+            document
+        }
+    }
+    
+}
